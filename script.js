a43203fc3d8db70966c004f2d388830bc2641eb9
GowthamDarkseid <123238178+Gowtham-Darkseid@users.noreply.github.com>
2024-09-02 19:27:33 +0530

    commit

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 
+    document.addEventListener("DOMContentLoaded", function() {
+        const typedText = document.querySelector(".typed-text");
+        const text = "Hey! I'm Gowtham Seenivasan";
+        let index = 0;
+
+        function typeWriter() {
+            if (index < text.length) {
+                typedText.textContent += text.charAt(index);
+                index++;
+                setTimeout(typeWriter, 100); // Adjust typing speed here
+            }
+        }
+
+        typeWriter();
+    });
+
+
+
+
